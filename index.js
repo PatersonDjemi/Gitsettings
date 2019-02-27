ecd2c014b110c365506732b1050ebf934f54e7d9
paterson <hassanpaterson@yahoo.com>
2019-02-27 22:08:22 +0100

    4e commit, ajoute juste index et laisse le fichier log

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const add = (a, b)=> {
 
 const result = add(1, 1);
 const result2 = add(2, 2);
+const result3 = add(3, 3);
 console.log("result is", result);
 console.log("result is", result2);
+console.log("result is", result3);
